7c87967af935e1fcd814d9ec37bbc0d2887f9d9e
Noé Delin <noe.delin.etu@univ-lille.fr>
2024-03-03 15:42:31 +0100

    LivingEntity has HP green test

--- src/entity/LivingEntity.js
+++ src/entity/LivingEntity.js
@@ -1,1 +1,8 @@
-export class LivingEntity {}
+import { DynamicEntity } from './DynamicEntity.js';
+
+export class LivingEntity extends DynamicEntity {
+	constructor(hp, x, y, width, height, color) {
+		super(x, y, width, height, color);
+		this.HP = hp;
+	}
+}
