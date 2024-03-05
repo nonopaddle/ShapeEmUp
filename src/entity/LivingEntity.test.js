e783633507df8a3f6c498499560616a73e957d69
Noe Delin <noe.delin.etu@univ-lille.fr>
2024-03-05 13:14:37 +0100

    LigingEntity must disappear when it dies red test

--- src/entity/LivingEntity.test.js
+++ src/entity/LivingEntity.test.js
@@ -1,6 +1,7 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import { LivingEntity } from './LivingEntity.js';
+import { GameArea } from '../GameArea.js';
 
 describe('LivingEntity tests', () => {
 	it('has HPs', () => {
@@ -22,5 +23,23 @@ describe('LivingEntity tests', () => {
 		const le = new LivingEntity(datas);
 		le.hurt(15);
 		assert.strictEqual(85, le.HP);
+	});
+
+	it("must disappear when it's hp reaches 0", () => {
+		const datas = {
+			pos: { x: 25, y: 25 },
+			size: { x: 40, y: 40 },
+			default_hp: 100,
+		};
+		const le = new LivingEntity(datas);
+
+		const ga = new GameArea();
+		ga.add_entity(le);
+
+		assert.strictEqual(1, ga.entities.length);
+		assert.strictEqual(le, ga.entities[0]);
+		le.hurt(100);
+		assert(!ga.entities.includes(le));
+		assert.strictEqual(0, ga.entities.length);
 	});
 });
