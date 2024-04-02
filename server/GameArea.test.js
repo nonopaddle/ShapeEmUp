8b07c04692b489ac0605fca87c9345721c2741f2
Noe Delin <noe.delin.etu@univ-lille.fr>
2024-04-02 20:37:40 +0200

    GameArea playerproof boundaries red test

--- server/GameArea.test.js
+++ server/GameArea.test.js
@@ -2,6 +2,8 @@ import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import { Entity } from './entity/Entity.js';
 import gameArea from './GameArea.js';
+import { PlayerEntity } from './entity/PlayerEntity.js';
+import { Vector2 } from './math/Vector2.js';
 
 describe('GameArea tests', () => {
 	it('should be able to add an entity', () => {
@@ -22,5 +24,29 @@ describe('GameArea tests', () => {
 		assert.strictEqual(10, gameArea.entities[1].pos.x);
 		assert.strictEqual(15, gameArea.entities[1].pos.y);
 		assert.strictEqual(15, gameArea.entities[1].radius);
+	});
+	it('must have playerproof boundaries', () => {
+		const player = new PlayerEntity({
+			pos: { x: 120, y: 130 },
+			radius: 25,
+		});
+		gameArea.add_entity(player);
+		assert.strictEqual(120, player.pos.x);
+		assert.strictEqual(130, player.pos.y);
+
+		player.apply_vector_once(new Vector2(-200, 0));
+		player.update();
+		assert.strictEqual(0, player.pos.x);
+		assert.strictEqual(130, player.pos.y);
+
+		player.apply_vector_once(new Vector2(0, -200));
+		player.update();
+		assert.strictEqual(0, player.pos.x);
+		assert.strictEqual(0, player.pos.y);
+
+		player.apply_vector_once(new Vector2(-200, -200));
+		player.update();
+		assert.strictEqual(0, player.pos.x);
+		assert.strictEqual(0, player.pos.y);
 	});
 });
