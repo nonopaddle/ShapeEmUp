f764d01fb5de915cdcff09527b16c5bb839ad715
Noé Delin <noe.delin.etu@univ-lille.fr>
2024-04-06 17:40:51 +0200

    SpawnerEntity multi spawn green test

--- server/entity/SpawnerEntity.js
+++ server/entity/SpawnerEntity.js
@@ -4,13 +4,11 @@ import gameArea from '../GameArea.js';
 import { randInt } from '../math/MathUtils.js';
 
 export class SpawnerEntity extends Entity {
-	static monsterNb = 0;
-	static maxMonster = 25;
-
+	nbMonsterPerSpawn = 4;
 	constructor(datas) {
 		super(datas);
 		this.radius = 5;
-		this.spawnCooldown = randInt(75, 200);
+		this.spawnCooldown = 0;
 	}
 
 	render(ctx) {
@@ -19,29 +17,29 @@ export class SpawnerEntity extends Entity {
 
 	update() {
 		this.hitbox.update();
-		if (SpawnerEntity.monsterNb < SpawnerEntity.maxMonster) {
-			this.spawnCooldown -= 1;
-			if (this.spawnCooldown <= 0) {
-				this.spawn_monster();
-				this.spawnCooldown = randInt(75, 200);
-			}
+		this.spawnCooldown -= 1;
+		if (this.spawnCooldown <= 0) {
+			this.spawn_monster();
+			this.spawnCooldown = randInt(75, 200);
 		}
 	}
 
 	spawn_monster() {
-		const randMult = randInt(1, 3);
-		const datas = {
-			pos: { x: this.pos.x, y: this.pos.y },
-			radius: 20 * (randMult / 1.5),
-			default_hp: 25 * randMult,
-			speedMult: 10 - randMult,
-			damage: 5,
-			color: 'red',
-			difficulty: gameArea.difficulty,
-		};
-		const players = gameArea.get_players();
-		const playerAgro = players[randInt(0, players.length - 1)];
-		gameArea.add_entity(new MonsterEntity(datas, playerAgro));
-		SpawnerEntity.monsterNb += 1;
+		console.log('update');
+		for (let i = 0; i < this.nbMonsterPerSpawn; i++) {
+			const randMult = randInt(1, 3);
+			const datas = {
+				pos: { x: this.pos.x, y: this.pos.y },
+				radius: 20 * (randMult / 1.5),
+				default_hp: 25 * randMult,
+				speedMult: 10 - randMult,
+				damage: 5,
+				color: 'red',
+				difficulty: gameArea.difficulty,
+			};
+			const players = gameArea.get_players();
+			const playerAgro = players[randInt(0, players.length - 1)];
+			gameArea.add_entity(new MonsterEntity(datas, playerAgro));
+		}
 	}
 }
